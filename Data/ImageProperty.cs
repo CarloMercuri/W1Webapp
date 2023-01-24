b9cbcf4c515400452349100c0710f7882ab1e7cb
Carlo Mercuri <cmercuri85@gmail.com>
2023-01-24 10:59:50 +0100

    first

---
+++ Data/ImageProperty.cs
@@ -0,0 +1,13 @@
+﻿namespace W1Webapp.Data
+{
+    public class ImageProperty
+    {
+        public string Name { get; set; }
+        public string Extension { get; set; }
+        public string Type { get; set; }
+        public string Dimensions { get; set; }
+        public long Size { get; set; }
+        public string OriginalAddress { get; set; }
+        public Guid Guid { get; set; }
+    }
+}
